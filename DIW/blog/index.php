5460d4f5e7b18963e45e668d519d1c254d3544a5
paco <pacors88@gmail.com>
2021-01-15 12:35:33 +0100

    apppeliculas updated

--- DIW/blog/index.php
+++ DIW/blog/index.php
@@ -1,72 +1,73 @@
 <?php
-    include_once('./layout/header.php');
-    include_once('./layout/separator.php');
-    include_once('./layout/smedia-float.php');
-    require_once('./database/connection.php');
+include_once('./layout/header.php');
+include_once('./layout/separator.php');
+include_once('./layout/smedia-float.php');
+require_once('./database/connection.php');
 
 ?>
 
-    <main>
-        <div id="index">
-            <section class="container about-me">
-                <div class="about-img">
-                    <img src="img/img-1.jpg" alt="frs">
-                </div>
-                <p>My name is Fran and I was born in 1988 in Vila-real, a village from Spain.</p>
-                <p>After a few years working as an electromechanical in the ceramic industry, I had discovered web developing. In 2019 I made an important step into my life, I quitted my job and then I started to learn more about developing at full time.
-                    I have been doing a few courses by myself and also a lot of steps learning new technologies that were unknown for me. At this moment I'm studying developing of web applications at Didáctica Ágil Centros Academy, and my next steps are
-                    deep learning about Angular and Ionic.</p>
-            </section>
+<main>
+    <div id="index">
+        <section class="container about-me">
+            <div class="about-img">
+                <img src="img/img-1.jpg" alt="frs">
+            </div>
+            <p>My name is Fran and I was born in 1988 in Vila-real, a village from Spain.</p>
+            <p>After a few years working as an electromechanical in the ceramic industry, I had discovered web developing. In 2019 I made an important step into my life, I quitted my job and then I started to learn more about developing at full time.
+                I have been doing a few courses by myself and also a lot of steps learning new technologies that were unknown for me. At this moment I'm studying developing of web applications at Didáctica Ágil Centros Academy, and my next steps are
+                deep learning about Angular and Ionic.</p>
+        </section>
 
-            <section class="separator container">
-                <h2>Projects</h2>
-            </section>
+        <section class="separator container">
+            <h2>Projects</h2>
+        </section>
 
-            <section class="container projects">
+        <section class="container projects">
 
-                <?php
-                    if($conn) {
+            <?php
+            if ($conn) {
 
-                        $query = "SELECT * FROM proyectos LIMIT 3";
+                $query = "SELECT * FROM proyectos LIMIT 3";
 
-                        if($result = $conn->query($query)) {
-                            while($row = $result->fetch_assoc()){ ?>
+                if ($result = $conn->query($query)) {
+                    while ($row = $result->fetch_assoc()) { ?>
 
-                                <div class="project">
-                                    <img src="<?php echo $row['portada']; ?>" alt="ss">
-                                    <span class="info-project"><?php echo $row['nombre']; ?></span>
-                                </div>
+                        <a href="<?php echo $row['url']; ?>" target="_blank" class="project">
 
-                            <?php
-                            }
-                        }
+                            <img src="<?php echo $row['portada']; ?>" alt="<?php echo 'img-' . $row['nombre']; ?>">
+                            <span class="info-project"><?php echo $row['nombre']; ?></span>
+                        </a>
+
+            <?php
                     }
+                }
+            }
 
+            ?>
+
+        </section>
+
+        <section class="separator container">
+            <h2>most related posts</h2>
+        </section>
+
+        <section id="most-relateds" class="container">
+
+            <div class="articles">
+                <?php
+                include_once('./layout/most-visited.php');
+                $conn->close();
                 ?>
 
-            </section>
+            </div>
 
-            <section class="separator container">
-                <h2>most related posts</h2>
-            </section>
+        </section>
+    </div>
 
-            <section id="most-relateds" class="container">
+</main>
+<!-- End Main Content -->
 
-                <div class="articles">
-                    <?php 
-                        include_once('./layout/most-visited.php'); 
-                        $conn->close();
-                    ?>
-
-                </div>
-
-            </section>
-        </div>
-
-    </main>
-    <!-- End Main Content -->
-    
-<?php 
-    include_once('./layout/clip.php');
-    include_once('./layout/footer.php');
+<?php
+include_once('./layout/clip.php');
+include_once('./layout/footer.php');
 ?>
--- DIW/blog/js/main.js
+++ DIW/blog/js/main.js
@@ -1,142 +1,142 @@
 $().ready(() => {
-    "use strict";
+   "use strict";
 
-    /* Show and Hide mobile menu*/
-    const burger = $(".burger");
-    const burgerMenu = $(".burger-menu");
-    const mobileMenu = $(".mobile-menu");
-    const closeMenu = $(".close-menu");
+   /* Show and Hide mobile menu*/
+   const burger = $(".burger");
+   const burgerMenu = $(".burger-menu");
+   const mobileMenu = $(".mobile-menu");
+   const closeMenu = $(".close-menu");
 
-    burger.mouseenter(() => {
-        let lineas = $(".linea");
-        for (let i = 0; i < lineas.length; i++) {
-            lineas[i].classList.add("line-colour");
-        }
-    });
+   burger.mouseenter(() => {
+      let lineas = $(".linea");
+      for (let i = 0; i < lineas.length; i++) {
+         lineas[i].classList.add("line-colour");
+      }
+   });
 
-    burger.mouseleave(() => {
-        let lineas = $(".linea");
-        for (let i = 0; i < lineas.length; i++) {
-            lineas[i].classList.remove("line-colour");
-        }
-    });
+   burger.mouseleave(() => {
+      let lineas = $(".linea");
+      for (let i = 0; i < lineas.length; i++) {
+         lineas[i].classList.remove("line-colour");
+      }
+   });
 
-    /* Add rotate effect to menu burger lines */
-    burgerMenu.click(() => {
-        if (mobileMenu.hasClass("show-menu")) {
-            removeEffect();
-        } else {
-            mobileMenu.addClass("show-menu");
-            $(".line-one").addClass("line-1");
-            $(".line-two").addClass("line-2");
-            $(".line-three").addClass("line-3");
-        }
-    });
+   /* Add rotate effect to menu burger lines */
+   burgerMenu.click(() => {
+      if (mobileMenu.hasClass("show-menu")) {
+         removeEffect();
+      } else {
+         mobileMenu.addClass("show-menu");
+         $(".line-one").addClass("line-1");
+         $(".line-two").addClass("line-2");
+         $(".line-three").addClass("line-3");
+      }
+   });
 
-    closeMenu.click(() => {
-        removeEffect();
-    });
-    /* Remove rotate effects to menu burger lines */
-    function removeEffect() {
-        mobileMenu.removeClass("show-menu");
-        $(".line-one").removeClass("line-1");
-        $(".line-two").removeClass("line-2");
-        $(".line-three").removeClass("line-3");
-    }
+   closeMenu.click(() => {
+      removeEffect();
+   });
+   /* Remove rotate effects to menu burger lines */
+   function removeEffect() {
+      mobileMenu.removeClass("show-menu");
+      $(".line-one").removeClass("line-1");
+      $(".line-two").removeClass("line-2");
+      $(".line-three").removeClass("line-3");
+   }
 
-    let nav = $("nav");
-    let frs = $(".frs");
+   let nav = $("nav");
+   let frs = $(".frs");
 
-    // Scroll functions
+   // Scroll functions
 
-    $(document).scroll(function () {
-        let separator = $(".separator").offset().top;
-        let position = $(window).scrollTop() + 120;
+   $(document).scroll(function () {
+      let separator = $(".separator").offset().top;
+      let position = $(window).scrollTop() + 120;
 
-        // Change navbar to white-bg and black-color
-        if (position > separator) {
-            nav.addClass("bg-white shadow black-text");
-            $(".a-nav").addClass("black");
-            frs.css("display", "inline");
-        } else {
-            nav.removeClass("bg-white shadow");
-            $(".a-nav").removeClass("black");
-            frs.css("display", "none");
-        }
+      // Change navbar to white-bg and black-color
+      if (position > separator) {
+         nav.addClass("bg-white shadow black-text");
+         $(".a-nav").addClass("black");
+         frs.css("display", "inline");
+      } else {
+         nav.removeClass("bg-white shadow");
+         $(".a-nav").removeClass("black");
+         frs.css("display", "none");
+      }
 
-        // GOTOP Button Hide/Show animation class
-        var height = $(window).scrollTop();
-        if (height > 600) {
-            $("#goTop").addClass("move");
-        } else {
-            $("#goTop").removeClass("move");
-        }
-    });
+      // GOTOP Button Hide/Show animation class
+      var height = $(window).scrollTop();
+      if (height > 600) {
+         $("#goTop").addClass("move");
+      } else {
+         $("#goTop").removeClass("move");
+      }
+   });
 
-    // Set transparent NAV only at index.*
-    let url = $(location).attr("href");
-    let page = url.split("/");
-    page = page[page.length - 1].split(".")[0];
+   // Set transparent NAV only at index.*
+   let url = $(location).attr("href");
+   let page = url.split("/");
+   page = page[page.length - 1].split(".")[0];
 
-    if (page != "index" && page != "") {
-        console.log(page);
-        nav.addClass("bg-white shadow black-text");
-        $(".a-nav").addClass("black");
-        frs.css("display", "inline");
-    }
+   if (page != "index" && page != "") {
+      console.log(page);
+      nav.addClass("bg-white shadow black-text");
+      $(".a-nav").addClass("black");
+      frs.css("display", "inline");
+   }
 
-    // GoTop movement to TOP
-    $("#goTop").click(function (event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, 1300);
-    });
+   // GoTop movement to TOP
+   $("#goTop").click(function (event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, 1300);
+   });
 
-    // Hover to projects images
-    let proyectos = $(".project");
+   // Hover to projects images
+   let proyectos = $(".project");
 
-    for (let i = 0; i < proyectos.length; i++) {
-        proyectos[i].addEventListener("mouseenter", () => {
-            proyectos[i].lastElementChild.classList.add("move-info");
-            proyectos[i].children[0].classList.add("filteroff");
-        });
-        proyectos[i].addEventListener("mouseleave", () => {
-            proyectos[i].lastElementChild.classList.remove("move-info");
-            proyectos[i].children[0].classList.remove("filteroff");
-        });
-    }
+   for (let i = 0; i < proyectos.length; i++) {
+      proyectos[i].addEventListener("mouseenter", () => {
+         proyectos[i].lastElementChild.classList.add("move-info");
+         proyectos[i].children[0].classList.add("filteroff");
+      });
+      proyectos[i].addEventListener("mouseleave", () => {
+         proyectos[i].lastElementChild.classList.remove("move-info");
+         proyectos[i].children[0].classList.remove("filteroff");
+      });
+   }
 
-    // form animation line
-    const nameInput = $("#input-name");
-    const focusName = $(".focus-name");
+   // form animation line
+   const nameInput = $("#input-name");
+   const focusName = $(".focus-name");
 
-    const emailInput = $("#input-email");
-    const focusEmail = $(".focus-email");
+   const emailInput = $("#input-email");
+   const focusEmail = $(".focus-email");
 
-    const spanUser = $(".focus-text-name");
-    const spanEmail = $(".focus-text-email");
+   const spanUser = $(".focus-text-name");
+   const spanEmail = $(".focus-text-email");
 
-    nameInput.focus(() => {
-        focusName.addClass("focus-dos");
-        spanUser.addClass("focus-dos-text");
-    });
+   nameInput.focus(() => {
+      focusName.addClass("focus-dos");
+      spanUser.addClass("focus-dos-text");
+   });
 
-    nameInput.blur(() => {
-        focusName.removeClass("focus-dos");
-        if (nameInput[0].value == 0) {
-            spanUser.removeClass("focus-dos-text");
-        }
-    });
+   nameInput.blur(() => {
+      focusName.removeClass("focus-dos");
+      if (nameInput[0].value == 0) {
+         spanUser.removeClass("focus-dos-text");
+      }
+   });
 
-    emailInput.focus(() => {
-        focusEmail.addClass("focus-dos");
-        spanEmail.addClass("focus-dos-text");
-    });
+   emailInput.focus(() => {
+      focusEmail.addClass("focus-dos");
+      spanEmail.addClass("focus-dos-text");
+   });
 
-    emailInput.blur(() => {
-        focusEmail.removeClass("focus-dos");
+   emailInput.blur(() => {
+      focusEmail.removeClass("focus-dos");
 
-        if (emailInput[0].value == 0) {
-            spanEmail.removeClass("focus-dos-text");
-        }
-    });
+      if (emailInput[0].value == 0) {
+         spanEmail.removeClass("focus-dos-text");
+      }
+   });
 });
